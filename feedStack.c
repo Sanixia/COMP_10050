7e76dbbf7f818760e2f6d7dbb456da603afbda91
glucoseIntolerant <michal.lanecki@ucdconnect.ie>
2022-05-03 22:58:37 +0100

    stack header and c file implementation

---
+++ feedStack.c
@@ -0,0 +1,5 @@
+//
+// Created by User01 on 03/05/2022.
+//
+
+#include "feedStack.h"
---
+++ feedStack.h
@@ -0,0 +1,8 @@
+//
+// Created by User01 on 03/05/2022.
+//
+
+#ifndef COMP_10050_FEEDSTACK_H
+#define COMP_10050_FEEDSTACK_H
+
+#endif //COMP_10050_FEEDSTACK_H
