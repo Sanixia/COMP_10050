924f852fd9445c9869602b6fd8125e22759438f8
Sanixia <roshan.varghese@ucdconnect.ie>
2022-05-04 16:48:49 +0100

    created a seperate unfollow user source file

---
+++ unfollow.c
@@ -0,0 +1,9 @@
+//
+// Created by chant on 04/05/2022.
+//
+
+#include <stdio.h>
+#include "followAndUnfollow.h"
+#include "twitter_create.h"
+
+void unfollow_user(twitter * twitter_system, int userNumber);
