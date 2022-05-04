532c29f77aed84e1267e1016f634ba6bd8078720
Sanixia <roshan.varghese@ucdconnect.ie>
2022-05-04 21:52:46 +0100

    test 5

--- main.c
+++ main.c
@@ -80,6 +80,7 @@ int main() {
             // Service 2 - Unfollow
             if (choice == 2)
             {
+
                 scanf("%c", (char *) stdin);
                 unfollow_user(&twitter_system, j);
             }
