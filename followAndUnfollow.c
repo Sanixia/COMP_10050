dc267ee0aca5472030d34774ff3939832ff640b0
Sanixia <roshan.varghese@ucdconnect.ie>
2022-05-04 16:44:24 +0100

    follow function progress(4, almost finished)

--- followAndUnfollow.c
+++ followAndUnfollow.c
@@ -8,34 +8,60 @@
 #include "followAndUnfollow.h"
 
 void follow_user(twitter * twitter_system, int userNumber){
-    int i;
+    int i, j, k;
+    int checkIfInList = 0, checkValidUsername = 0;
     char name[30];
 
 
-    printf("\nHere is a list of users you haven't followed yet:\n");
+    printf("\nHere is a list of users you haven't followed yet:");
 
     for (i = 0; i < twitter_system->num_users; i++){
-        if ( (strcmp( twitter_system->max_users[userNumber].following[i], twitter_system->max_users[i].username) != 0) && (strcmp( twitter_system->max_users[userNumber].username, twitter_system->max_users[i].username) != 0)){
-            printf("%s\n",twitter_system->max_users[i].username );
+
+        if (strcmp( twitter_system->max_users[userNumber].username, twitter_system->max_users[i].username) != 0){
+            checkIfInList = 0;
+
+            for (j = 0; j < MAX_FOLLOWING; j++){
+                if (strcmp( twitter_system->max_users[userNumber].following[j], twitter_system->max_users[i].username) == 0){
+                    checkIfInList = 1;
+                    j = MAX_FOLLOWING;
+                }
+            }
+
+            if (checkIfInList == 0){
+                printf("\nUser %d: %s",i , twitter_system->max_users[i].username );
+            }
         }
     }
 
-    printf("\nPlease enter the username of the person you want to follow: ");
 
-    fgets(name, USR_LENGTH, stdin);
+    if (checkIfInList == 1){
+        printf("\nExcept it looks like you followed everyone!\n");
+    }
 
-    printf("%s", name);
+    else{
+
+        while(checkValidUsername == 0){
+            printf("\n\nPlease enter the valid username of the person you want to follow: ");
+
+            fgets(name, USR_LENGTH, stdin);
+
+            if(name[strlen(name) - 1] == '\n'){
+                name[strlen(name) - 1] = '\0';      //replaces the end of each input with \0 instead of \n otherwise it wouldn't work correctly
+            }
+
+            for(k = 0;k < twitter_system->num_users;k++){
+                if (strcmp(twitter_system->max_users[k].username, name) == 0){
+                    checkValidUsername = 1;
+                    k = twitter_system->num_users;
+                }
+            }
+        }
 
 
-    strcpy( twitter_system->max_users[userNumber].following[twitter_system->max_users[userNumber].num_following], name);
-    twitter_system->max_users[userNumber].num_following++;
-
-
-
-
-
-    printf("\n%s", (twitter_system->max_users[userNumber].following[twitter_system->max_users[userNumber].num_following - 1]));
-    printf("\n%d",(twitter_system->max_users[userNumber].num_following));
+        strcpy( twitter_system->max_users[userNumber].following[twitter_system->max_users[userNumber].num_following], name);
+        twitter_system->max_users[userNumber].num_following++;
+        printf("\n%s has been added to your following list!\n", name);
+    }
 
 }
 
